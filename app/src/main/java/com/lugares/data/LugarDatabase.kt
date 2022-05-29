41b7eea2936a29758829f0e6ce22d0bb8203f8f7
Zuz <mighnightjht@gmail.com>
2022-05-28 23:46:37 -0600

    Version de lugares (Semana2)

---
+++ app/src/main/java/com/lugares/data/LugarDatabase.kt
@@ -0,0 +1,32 @@
+package com.lugares.data
+
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.lugares.model.Lugar
+
+@Database(entities = [Lugar::class], version = 1, exportSchema = false)
+abstract class LugarDatabase: RoomDatabase() {
+    abstract fun lugarDao() : LugarDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: LugarDatabase? = null
+
+        fun getDataBase(context: android.content.Context) : LugarDatabase {
+            val temp = INSTANCE
+            if (temp != null){
+                return temp
+            }
+            synchronized(this){
+                val intance = Room.databaseBuilder(
+                    context.applicationContext,
+                    LugarDatabase::class.java,
+                    "lugar_database"
+                ).build()
+                INSTANCE=intance
+                return intance
+            }
+        }
+    }
+}
