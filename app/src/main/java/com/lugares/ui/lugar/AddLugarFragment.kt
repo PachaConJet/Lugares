3a8490114221787f6c585e70488f41ce8c215879
Zuz <mighnightjht@gmail.com>
2022-06-03 16:05:34 -0600

    Version de lugares (Semana3)

--- app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
@@ -1,18 +1,20 @@
 package com.lugares.ui.lugar
 
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.lugares.R
 import com.lugares.databinding.FragmentLugarBinding
+import com.lugares.model.Lugar
 import com.lugares.viewmodel.LugarViewModel
 
-class LugarFragment : Fragment() {
+class AddLugarFragment : Fragment() {
 
     private var _binding: FragmentLugarBinding? = null
 
@@ -27,17 +29,34 @@ class LugarFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-         lugarViewModel =
+        lugarViewModel =
             ViewModelProvider(this).get(LugarViewModel::class.java)
 
         _binding = FragmentLugarBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        binding.addLugar.setOnClickListener{
-            findNavController().navigate(R.id.action_nav_lugar_to_addLugarFrag )
+      binding.btAgregar.seOnClickListener{
+            addLugar()
         }
-
         return root
+
+    }
+
+    private fun addLugar() {
+        val nombre =  binding.etNombre.text.toString
+        val correo =  binding.etCorreo.text.toString
+        val telefono =  binding.etTelefono.text.toString
+        val web =  binding.etWeb.text.toString
+        if(nombre.isNotEmpty()) {
+            //Si puedo crear un lugar
+            val lugar = Lugar(0,nombre,correo,telefono,web,0.0,"","","","")
+            lugarViewModel.addLugar(lugar)
+            Toast.makeText(requireContext(),getString(R.string.msg_lugar_added),Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_addLugarFrag_to_nav_lugar)
+        }else{
+            //Mensaje de error
+            Toast.makeText(requireContext(),getString(R.string.msg_data),Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
