1411f73d02d0afb3c9da9b67e63d5f25cdd7dcdc
Zuz <mighnightjht@gmail.com>
2022-06-13 16:49:58 -0600

    Version de lugares (Semana4)

--- app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.lugares.R
+import com.lugares.adapter.LugarAdapter
 import com.lugares.databinding.FragmentLugarBinding
 import com.lugares.viewmodel.LugarViewModel
 
@@ -35,6 +36,15 @@ class LugarFragment : Fragment() {
 
         binding.addLugar.setOnClickListener{
             findNavController().navigate(R.id.action_nav_lugar_to_addLugarFrag )
+        }
+
+        //Activar el reciclador
+        val lugarAdapter = LugarAdapter()
+        val reciclador = binding.reciclador
+        reciclador.adapter = lugarAdapter
+        reciclador.layoutManager = LinearLayoutManager(requireContext())
+        lugarViewModel.getAllData.observe(viewLifecycleOwner){
+            lugarAdapter.setData(it)
         }
 
         return root
--- app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
@@ -8,12 +8,18 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.lugares.R
-import com.lugares.databinding.FragmentAddLugarBinding
+import com.lugares.databinding.FragmentUpdateLugarBinding
 import com.lugares.model.Lugar
 import com.lugares.viewmodel.LugarViewModel
-class AddLugarFragment : Fragment() {
-    private var _binding: FragmentAddLugarBinding? = null
+class UpdateLugarFragment : Fragment() {
+
+    //Se reciben los parametros pasados por argumento
+    private val args by navArgs<UpdateLugarFragmentArgs>()
+
+
+    private var _binding: FragmentUpdateLugarBinding? = null
     private val binding get() = _binding!!
     private lateinit var lugarViewModel: LugarViewModel
     override fun onCreateView(
@@ -23,24 +29,33 @@ class AddLugarFragment : Fragment() {
     ): View {
         lugarViewModel =
             ViewModelProvider(this)[LugarViewModel::class.java]
-        _binding = FragmentAddLugarBinding.inflate(inflater, container, false)
-        binding.btAgregar.setOnClickListener {
-            addLugar()
+        _binding = FragmentUpdateLugarBinding.inflate(inflater, container, false)
+
+        // Colocar la info del lugar en los campos del fragmento para modificar
+        binding.etNombre.setText(args.lugar.nombre)
+        binding.etCorreo.setText(args.lugar.correo)
+        binding.etTelefono.setText(args.lugar.telefono)
+        binding.etWeb.setText(args.lugar.web)
+
+        binding.btActualizar.setOnClickListener {
+            updateLugar()
         }
         return binding.root
     }
 
-    private fun addLugar() {
+    private fun updateLugar() {
         val nombre=binding.etNombre.text.toString()
         val correo=binding.etCorreo.text.toString()
         val telefono=binding.etTelefono.text.toString()
         val web=binding.etWeb.text.toString()
         if (nombre.isNotEmpty()) { //Si puedo crear un lugar
-            val lugar= Lugar(0,nombre,correo,telefono,web,0.0,
+            val lugar= Lugar(args.lugar.id,nombre,correo,telefono,web,0.0,
                 0.0,0.0,"","")
-            lugarViewModel.addLugar(lugar)
-            Toast.makeText(requireContext(),getString(R.string.msg_lugar_added),Toast.LENGTH_SHORT).show()
-            findNavController().navigate(R.id.action_addLugarFrag_to_nav_lugar)
+
+            lugarViewModel.updateLugar(lugar)
+
+            Toast.makeText(requireContext(),getString(R.string.msg_lugar_update),Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_updateLugarFragment_to_nav_lugar)
         } else {  //Mensaje de error...
             Toast.makeText(requireContext(),getString(R.string.msg_data),Toast.LENGTH_SHORT).show()
         }
